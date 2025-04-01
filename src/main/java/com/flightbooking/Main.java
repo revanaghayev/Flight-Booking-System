c41d9a596f47f205ccbe3ab21482503d58b28a59
gunelmirzoeva <mirzoevagunel20@gmail.com>
2025-04-01 18:53:31 +0400

    Add Flight model class

--- src/main/java/com/flightbooking/Main.java
+++ src/main/java/com/flightbooking/Main.java
@@ -1,4 +1,13 @@
 package com.flightbooking;
+//
+//import com.flightbooking.model.Flight;
+//
+//import java.time.LocalDateTime;
 
 public class Main {
+//    public static void main(String[] args) {
+//        Flight flight = new Flight(1, LocalDateTime.of(2025, 4, 2, 10, 30), "Paris",
+//                12, 5);
+//        System.out.println(flight);
+//    }
 }
--- src/main/java/com/flightbooking/model/Flight.java
+++ src/main/java/com/flightbooking/model/Flight.java
@@ -1,4 +1,50 @@
 package com.flightbooking.model;
 
+import java.time.LocalDateTime;
+
 public class Flight {
+    private long flightID;
+    private LocalDateTime departureTime;
+    private String destination;
+    private int totalSeats;
+    private int availableSeats;
+
+    public Flight(long flightID, LocalDateTime departureTime, String destination, int totalSeats, int availableSeats) {
+        this.flightID = flightID;
+        this.departureTime = departureTime;
+        this.destination = destination;
+        this.totalSeats = totalSeats;
+        this.availableSeats = availableSeats;
+    }
+
+    public long getFlightID() {
+        return flightID;
+    }
+
+    public LocalDateTime getDepartureTime() {
+        return departureTime;
+    }
+
+    public String getDestination() {
+        return destination;
+    }
+
+    public int getTotalSeats() {
+        return totalSeats;
+    }
+
+    public int getAvailableSeats() {
+        return availableSeats;
+    }
+    public void setAvailableSeats(int availableSeats) {
+        this.availableSeats = availableSeats;
+    }
+    @Override
+    public String toString() {
+        return "\n\n-----------FLIGHT INFO----------\n" +
+                "Flight ID: " + flightID +
+                "\nDeparture: " + departureTime +
+                "\nDestination: " + destination +
+                "\nAvailable Seats: " + availableSeats;
+    }
 }
