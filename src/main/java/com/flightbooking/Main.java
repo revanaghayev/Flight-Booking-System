f0022fa08499481fb2e6e84564934888e673fc83
revanaghayev <rvanagayev17@gmail.com>
2025-04-10 18:04:32 +0400

    Saving final changes

--- src/main/java/com/flightbooking/Main.java
+++ src/main/java/com/flightbooking/Main.java
@@ -4,13 +4,13 @@ import com.flightbooking.controller.BookingController;
 import com.flightbooking.dao.BookingDAO;
 import com.flightbooking.dao.BookingDAOImpl;
 import com.flightbooking.model.Booking;
+import com.flightbooking.model.Flight;
 import com.flightbooking.service.BookingService;
+
+import java.util.List;
 
 public class Main {
 
     public static void main(String[] args) {
-        Booking booking = new Booking();
-//        BookingDAO bookingDAO = new Booking
-        BookingController bookingController = new BookingController(booking)
     }
 }
